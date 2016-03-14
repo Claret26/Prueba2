88eb02f4bb3f09ca6f4032e240fde85dac211a08
Jorge <jorge.art.castillo@gmail.com>
2016-03-13 21:39:32 -0600

    3er

---
+++ TablasMultilicarTarea/build/web/Domain/University.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain;
+
+/**
+ *
+ * @author Jorge Castillo Vindas/jorge.castillovindas@ucr.ac.cr
+ */
+public class University {
+
+    private String countryName;
+    
+    
+    public University() {
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+    
+    
+    
+}
